47b2ac8fe8b45cbcaccc3a345cdfe09a46ec9f77
Lihuanghe <jameslover121@163.com>
2022-12-17 01:32:37 +0800

    使用git action进行压力测试100000

--- src/test/java/com/zx/sms/connect/manager/TestConstants.java
+++ src/test/java/com/zx/sms/connect/manager/TestConstants.java
@@ -1,5 +1,7 @@
 package com.zx.sms.connect.manager;
 
+import com.zx.sms.config.PropertiesUtils;
+
 public final class TestConstants {
-	public static final Integer Count = 30000; 
+	public static final Integer Count = Integer.parseInt(PropertiesUtils.getproperties("TestConstants.Count", "100000"));
 }
